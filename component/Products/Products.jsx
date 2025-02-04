04eaf237fd25b1c136f859ab5d654a63dc7bb339
Adinath Gaware <139313629+adinath302@users.noreply.github.com>
2025-02-04 23:02:54 +0530

    Add files via upload

---
+++ component/Products/Products.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import Heading from '../Services/Heading'
+
+const Products = () => {
+    return (
+        <div className="">
+            <div className='container'>
+                {/* Header section */}
+                <Heading title="Our Products"
+                    subtitle={"Explore Our Products"}
+                />
+                {/* body section */}
+            </div>
+        </div>
+    )
+}
+
+export default Products
